5db832837b9c24793d297bc67f39f4ee5c86b199
Ioannis Varouchakis <johnvarouchakis@gmail.com>
2024-01-23 19:56:01 +0100

    add some inputs

--- src/components/ExampleForm.js
+++ src/components/ExampleForm.js
@@ -27,9 +27,46 @@ const getStyles = () => (`
 const getTemplate = (values) => (`
     <section class="container">
         <slot name="header"></slot>
-        <form>
-            <label for="test">Test</label>
-            <input type="text" id="test" name="test" value="${values.test}">
+        <form onsubmit="this.getRootNode().host.submit(event, this)">
+            <label>
+                Test
+                <input type="text" name="test" value="${values.form.test}">
+            </label>
+            <label>
+                nested A
+                <input type="text" name="nested.a">
+            </label>
+            <label>
+                nested B
+                <input type="text" name="nested.b">
+            </label>
+
+            <label>
+                Array [0]
+                <input type="text" name="array[0]">
+            </label>
+
+            <label>
+                Array [1]
+                <input type="text" name="array[1]">
+            </label>
+
+            <label>
+                Multi Select
+                <select name="multi" multiple>
+                    <optgroup label="Group 1">
+                        <option value="a">A</option>
+                        <option value="b">B</option>
+                    </optgroup>
+                    <optgroup label="Group 2">
+                        <option value="c">C</option>
+                        <option value="d">D</option>
+                    </optgroup>
+                </select>
+            </label>
+            <br />
+
+            <button type="submit">Submit</button>
         </form>
     </section>
 `);
@@ -38,11 +75,34 @@ const getTemplate = (values) => (`
 const styleSheet = new CSSStyleSheet();
 styleSheet.replaceSync(getStyles());
 
+const addKeyMutate = (obj, key, value) => {
+    const isArray = key.match(/\[[0-9]+\]$/);
+    const isObject = key.match(/\.[a-z]+$/);
+
+    if (isArray) {
+        const arrayKey = key.replace(/\[[0-9]+\]$/, '');
+        const arrayIndex = key.match(/\[([0-9]+)\]$/)[1];
+        if (!obj[arrayKey]) {
+            obj[arrayKey] = [];
+        }
+        obj[arrayKey][arrayIndex] = value;
+    } else if (isObject) {
+        const objectKey = key.replace(/\.[a-z]+$/, '');
+        const objectSubKey = key.match(/\.[a-z]+$/)[0].replace(/^\./, '');
+        if (!obj[objectKey]) {
+            obj[objectKey] = {};
+        }
+        obj[objectKey][objectSubKey] = value;
+    } else {
+        obj[key] = value;
+    }
+};
+
 export class ExampleForm extends HTMLElement {
     static observedAttributes = ['test:'];
 
     #values = {
-        test: 'test',
+        form: { test: 'test' },
     }
 
     constructor() {
@@ -62,6 +122,23 @@ export class ExampleForm extends HTMLElement {
 
     render() {
         updateDom(this.shadowRoot, getTemplate(this.#values));
+    }
+
+    submit(event, form) {
+        event.preventDefault();
+        const elements= event.currentTarget.elements;
+        const values = {};
+        for (let i = 0; i < elements.length; i++) {
+            const isMultiSelect = elements[i].tagName === 'SELECT' && elements[i].multiple;
+
+            let currentValue = isMultiSelect
+                ? Array.from(elements[i].selectedOptions).map((v) => v.value)
+                : elements[i].value;
+
+            addKeyMutate(values, elements[i].name, currentValue);
+        }
+
+        console.log(values, elements);
     }
 }
 
